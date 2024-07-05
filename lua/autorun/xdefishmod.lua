4666a8f8cf847cffac81c86f77f71cd3650c94b3
C4ClaymoreV <63988663+C4ClaymoreV@users.noreply.github.com>
2024-07-05 03:29:07 -0400

    updated fisherman spawn functions

--- lua/autorun/xdefishmod.lua
+++ lua/autorun/xdefishmod.lua
@@ -3423,40 +3423,43 @@ elseif typ == 8 then -- 图鉴
 			local slo = xdefm_SlotBox( 0, 0, 100, 100, 0, nil, xdefm.miscs.Icons[ tab.Type ], true )
 			if slo then slo:F_SetupItem( ite[ i ] ) pa2:Add( slo )
 				--[[
-				function slo:DoClick() if !ply:IsAdmin() then return end local it = cls -- functions in charge of spawning items
+				function slo:DoClick() if !ply:IsSuperAdmin() then return end local it = cls -- functions in charge of spawning items
 					if tab.Type == "Food" then it = it.."|"..tab.BestCook
 					elseif tab.Type == "Creature" then it = it.."|"..tab.MaxSize
 					elseif tab.Type == "Recipe" then it = it.."|"..tab.Durability end
 					surface.PlaySound( "garrysmod/ui_click.wav" )
 					RunConsoleCommand( "xdefmod_spawn", it )
 				end
-				function slo:DoRightClick() if !ply:IsAdmin() then return end local it = cls
+				function slo:DoRightClick() if !ply:IsSuperAdmin() then return end local it = cls
 					if tab.Type == "Food" then it = it.."|"..tab.BestCook
 					elseif tab.Type == "Creature" then it = it.."|"..tab.MaxSize
 					elseif tab.Type == "Recipe" then it = it.."|"..tab.Durability end
 					surface.PlaySound( "garrysmod/ui_return.wav" )
 					RunConsoleCommand( "xdefmod_give", it )
-				end]]
-				local s_tbl = {
-					Food = cls.."|"..tab.BestCook,
-					Creature = cls.."|"..tab.MaxSize,
-					Recipe = cls.."|"..tab.Durability 
-				}
+				end--]]
 				function slo:DoClick()
 					if !ply:IsSuperAdmin() then return end
 					local it = cls
-					local s_tbl = s_tbl -- local to reduce lookup time
-					if s_tbl[tab.Type] then it = s_tbl[tab.Type] end
-
+					if tab.Type == "Food" then 
+						it = it.."|"..tab.BestCook
+					elseif tab.Type == "Creature" then 
+						it = it.."|"..tab.MaxSize
+					elseif tab.Type == "Recipe" then 
+						it = it.."|"..tab.Durability 
+					end
 					surface.PlaySound( "garrysmod/ui_click.wav" )
 					RunConsoleCommand( "xdefmod_spawn", it )
 				end
 				function slo:DoRightClick() 
 					if !ply:IsSuperAdmin() then return end 
 					local it = cls
-					local s_tbl = s_tbl
-					if s_tbl[tab.Type] then it = s_tbl[tab.Type] end
-
+					if tab.Type == "Food" then 
+						it = it.."|"..tab.BestCook
+					elseif tab.Type == "Creature" then 
+						it = it.."|"..tab.MaxSize
+					elseif tab.Type == "Recipe" then 
+						it = it.."|"..tab.Durability 
+					end
 					surface.PlaySound( "garrysmod/ui_return.wav" )
 					RunConsoleCommand( "xdefmod_give", it )
 				end
